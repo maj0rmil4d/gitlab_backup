e1a4ec3a5829fa310594eb31518324074ceb1b8b
Milad Soltanian <maj0rmil4d@gmail.com>
2022-02-07 13:00:27 +0330

    Create gitbk.sh

---
+++ gitbk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/:/usr/local/bin/:/root/gitlab_BK
+
+gitlab-project-export.py -c /root/gitlab_BK/gitlab-project-export/gitlab-project-export.config.yaml
